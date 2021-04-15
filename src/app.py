0f515545bc53e2b6a355764caee7f258337d907a
Smit Jain <scj39@cornell.edu>
2021-04-14 22:33:07 -0700

    added flask integration, all dataset augmentation done

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Dropout
+from tensorflow.keras.layers import Conv2DTranspose, Concatenate
 from flask import Flask, request
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -9,9 +12,14 @@ app = Flask(__name__)
 def inference():
     data = request.json
     img_arr = np.array(data["image"], dtype=np.uint8)
-    # TODO: Replace with a call to your model
-    random_mask = (np.random.uniform(size=(img_arr.shape[:2])) > 0.5).astype(np.uint8)
-    return {"prediction": random_mask.tolist()}
+    
+    # img_arr should be in format (x, x, 3)- rgb
+    img_arr = tf.convert_to_tensor(img_arr, dtype=np.uint8)
+    img_arr = img_arr[tf.newaxis, ...] # prepend so is (1, x, x, 3)
+    unet = tf.keras.models.load_model('unet.h5')
+    mask = unet.predict(img_arr)
+    return {"prediction": mask.tolist()}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
